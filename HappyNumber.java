40ae6b3713d2d05eca16ae6b64d70a94254a3bbe
Nishant28-sh <142868452+Nishant28-sh@users.noreply.github.com>
2026-06-18 10:59:55 +0530

    Approach2 Happy Number

--- HappyNumber.java
+++ HappyNumber.java
@@ -1,23 +1,52 @@
-import java.util.HashSet;
+// import java.util.HashSet;
+// class Solution {
+
+//     public boolean isHappy(int n) {
+//         HashSet<Integer> set = new HashSet<>();
+
+//         while (n != 1 && !set.contains(n)) {
+
+//             set.add(n);
+//             n = getSum(n);
+//         }
+//         return n == 1;
+//     }
+//     private int getSum(int n) {
+//         int sum = 0;
+//         while (n > 0) {
+//             int digit = n % 10;
+//             sum += digit * digit;
+//             n /= 10;
+//         }
+//         return sum;
+//     }
+// }
+
 class Solution {
 
     public boolean isHappy(int n) {
-        HashSet<Integer> set = new HashSet<>();
 
-        while (n != 1 && !set.contains(n)) {
+        int slow = n;
+        int fast = n;
 
-            set.add(n);
-            n = getSum(n);
-        }
-        return n == 1;
+        do {
+            slow = getSum(slow);
+            fast = getSum(getSum(fast));
+        } while (slow != fast);
+
+        return slow == 1;
     }
+
     private int getSum(int n) {
+
         int sum = 0;
+
         while (n > 0) {
             int digit = n % 10;
             sum += digit * digit;
             n /= 10;
         }
+
         return sum;
     }
 }
